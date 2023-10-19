a147891bad3a370e6052bb9976a7cbe480b39bc1
M-HamzaKhan295 <mhamzakhan295@gamil.com>
2023-10-19 14:25:34 +0500

    edit Uinput

--- 1.py
+++ 1.py
@@ -1,14 +1,13 @@
-a = float(input('Enter first side: '))  
-b = float(input('Enter second side: '))  
-c = float(input('Enter third side: '))  
+a = float(input('Plz enter first side: '))  
+b = float(input('Plz enter second side: '))  
+c = float(input('Plz enter third side: '))  
   
 # calculate the semi-perimeter  
 s = (a + b + c) / 2  
   
 # calculate the area  
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5  
-print('The area of the triangle is %0.2f' %area)  
+print('The area of the triangle is %0.2f:' %area)  
 
 print("Hello world")
-print("Hi")
 
